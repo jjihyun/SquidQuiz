22004fc647561c7c668c9a506a9f8839de1cef96
ChoiOneDay <rodnstk135@gmail.com>
2021-11-02 18:24:46 +0900

    에러 수정

--- SquidQuiz/src/main/java/com/pj/ptsd/quiz/service/logic/QuizServiceImpl.java
+++ SquidQuiz/src/main/java/com/pj/ptsd/quiz/service/logic/QuizServiceImpl.java
@@ -1,5 +1,28 @@
 package com.pj.ptsd.quiz.service.logic;
 
-public class QuizServiceImpl {
+import org.springframework.stereotype.Service;
+
+import com.pj.ptsd.quiz.domain.Ox;
+import com.pj.ptsd.quiz.service.QuizService;
+@Service
+public class QuizServiceImpl implements QuizService{
+
+	@Override
+	public int registerOxQuiz(Ox ox) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modifyOxQuiz(Ox ox) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeOxQuiz(int oxNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- SquidQuiz/src/main/java/com/pj/ptsd/quiz/store/logic/QuizStoreLogic.java
+++ SquidQuiz/src/main/java/com/pj/ptsd/quiz/store/logic/QuizStoreLogic.java
@@ -1,5 +1,29 @@
 package com.pj.ptsd.quiz.store.logic;
 
-public class QuizStoreLogic {
+import org.springframework.stereotype.Repository;
+
+import com.pj.ptsd.quiz.domain.Ox;
+import com.pj.ptsd.quiz.store.QuizStore;
+
+@Repository
+public class QuizStoreLogic implements QuizStore{
+
+	@Override
+	public int insertOxQuiz(Ox ox) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateOxQuiz(Ox ox) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteOxQuiz(int oxNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
