f9510d1c675916e0e04a522606d5dfaab21a7f9f
jiyoung9811 <20171309@vision.hoseo.edu>
2021-11-02 17:27:56 +0900

    캠페인 리스트 조회 코딩중, mapper 2021.11.02

--- SquidQuiz/src/main/java/com/pj/ptsd/campaign/store/CampaignStore.java
+++ SquidQuiz/src/main/java/com/pj/ptsd/campaign/store/CampaignStore.java
@@ -1,5 +1,11 @@
 package com.pj.ptsd.campaign.store;
 
+import java.util.List;
+
+import com.pj.ptsd.campaign.domain.Campaign;
+
 public interface CampaignStore {
+
+	public List<Campaign> selectAll();
 
 }
