94b037de74a428b2a0a1c53dd43183d6625cb634
OHYEONJONG <callmejjong@gmail.com>
2021-11-04 14:26:44 +0900

    세번째-자유게시판

--- SquidQuiz/src/main/java/com/pj/ptsd/board/service/BoardService.java
+++ SquidQuiz/src/main/java/com/pj/ptsd/board/service/BoardService.java
@@ -1,14 +1,29 @@
 package com.pj.ptsd.board.service;
 
+import java.util.List;
+
+
 import com.pj.ptsd.board.domain.Board;
+import com.pj.ptsd.board.domain.PageInfo;
+import com.pj.ptsd.board.domain.Reply;
+import com.pj.ptsd.board.domain.Search;
+
 
 public interface BoardService {
-
 	
+	public List<Board> printAll(PageInfo pi);
+	public List<Board> selectSearchAll(Search search);	
+	
+	
+	public Board printOne(int boardNo);
 	public int registerBoard(Board board);
 	public int modifyBoard(Board board);
-	public int removeBoard(Board board);
-	//removeBoard int boardNo 생각해보기
+	public int removeBoard(int boardNo);
 	
+	public Board printreply(int replyNo);
+	public List<Reply> printAllReply(int boardNo);
+	public int registerReply(Reply reply);
+	public int modifyReply(Reply reply);
+	public int removeReply(Reply reply);
 	
 }
--- SquidQuiz/src/main/java/com/pj/ptsd/board/store/BoardStore.java
+++ SquidQuiz/src/main/java/com/pj/ptsd/board/store/BoardStore.java
@@ -1,5 +1,26 @@
 package com.pj.ptsd.board.store;
 
+import java.util.List;
+
+
+import com.pj.ptsd.board.domain.Board;
+import com.pj.ptsd.board.domain.PageInfo;
+import com.pj.ptsd.board.domain.Reply;
+import com.pj.ptsd.board.domain.Search;
+
 public interface BoardStore {
+	
+	public List<Board> selectAll(PageInfo pi);
+	public List<Board> selectSearchAll(Search search);	
+	
+	
+	public int insertBoard(Board board);
+	public int updateBoard(Board board);
+	public int deleteBoard(int boardNo);
+	
+	public List<Reply> selectAllReply(int boardNo);
+	public int insertReply(Reply reply);
+	public int updateReply(Reply reply);
+	public int deleteReply(Reply reply);
 
 }
