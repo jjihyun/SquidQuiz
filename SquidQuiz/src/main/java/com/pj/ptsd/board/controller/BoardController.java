56fdd307b16021a48a673f7da01c706eb176df19
OHYEONJONG <callmejjong@gmail.com>
2021-11-08 17:25:54 +0900

    네번째_자유게시판(추가)

--- SquidQuiz/src/main/java/com/pj/ptsd/board/controller/BoardController.java
+++ SquidQuiz/src/main/java/com/pj/ptsd/board/controller/BoardController.java
@@ -1,8 +1,21 @@
 package com.pj.ptsd.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pj.ptsd.board.service.BoardService;
+
+
+@Controller
 public class BoardController {
 	
-	
+	//게시글 작성
+	@RequestMapping(value="boardWrite",method=RequestMethod.GET)
+	public String boardWrite() {
+		return "board/boardWrie";
+	}
 	
 	
 	
@@ -11,3 +24,21 @@ public class BoardController {
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
